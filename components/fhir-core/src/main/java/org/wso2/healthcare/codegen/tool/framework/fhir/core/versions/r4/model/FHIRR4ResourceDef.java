a599ccf4252adf2848378b185d90f7764c3c1b5c
SA-Tester <siattanayake@gmail.com>
2025-04-28 16:13:04 +0530

    changed the centralized resource definitions to be accessed from both fhir r4 and r5

--- components/fhir-core/src/main/java/org/wso2/healthcare/codegen/tool/framework/fhir/core/versions/r4/model/FHIRR4ResourceDef.java
+++ components/fhir-core/src/main/java/org/wso2/healthcare/codegen/tool/framework/fhir/core/versions/r4/model/FHIRR4ResourceDef.java
@@ -21,25 +21,27 @@ package org.wso2.healthcare.codegen.tool.framework.fhir.core.versions.r4.model;
 import org.hl7.fhir.r4.model.ElementDefinition;
 import org.hl7.fhir.r4.model.Extension;
 import org.hl7.fhir.r4.model.StructureDefinition;
+import org.wso2.healthcare.codegen.tool.framework.fhir.core.model.FHIROperationDef;
 import org.wso2.healthcare.codegen.tool.framework.fhir.core.model.FHIRResourceDef;
+import org.wso2.healthcare.codegen.tool.framework.fhir.core.model.FHIRSearchParamDef;
 import org.wso2.healthcare.codegen.tool.framework.fhir.core.versions.r4.common.FHIRR4SpecUtils;
-import org.wso2.healthcare.codegen.tool.framework.fhir.core.versions.r4.util.R4DefKind;
-import org.wso2.healthcare.codegen.tool.framework.fhir.core.versions.r4.util.R4ElementExpansionType;
+import org.wso2.healthcare.codegen.tool.framework.fhir.core.util.DefKind;
+import org.wso2.healthcare.codegen.tool.framework.fhir.core.util.ElementExpansionType;
 
+import java.util.List;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
- * This class holds the FHIR resource definition.
+ * This class holds the FHIR R4 resource definition.
  */
-public class FHIRR4ResourceDef extends FHIRResourceDef {
+public class FHIRR4ResourceDef implements FHIRResourceDef <StructureDefinition, Extension, ElementDefinition> {
 
     private StructureDefinition definition;
-    private R4DefKind kind;
-    private final Map<String, FHIRR4SearchParamDef> searchParamDefs = new HashMap<>();
-    private final Map<String, FHIRR4OperationDef> operationDefMap = new HashMap<>();
+    private DefKind kind;
+    private final Map<String, FHIRSearchParamDef> searchParamDefs = new HashMap<>();
+    private final Map<String, FHIROperationDef> operationDefMap = new HashMap<>();
     private FHIRR4ResourceDef parentResource;
 
     @Override
@@ -47,32 +49,42 @@ public class FHIRR4ResourceDef extends FHIRResourceDef {
         return definition;
     }
 
+    @Override
     public void setDefinition(StructureDefinition definition) {
         this.definition = definition;
     }
 
+    @Override
     public List<Extension> getExtensions() {
         return definition.getExtension();
     }
 
-    public Map<String, FHIRR4SearchParamDef> getSearchParameters() {
+    @Override
+    public Map<String, FHIRSearchParamDef> getSearchParameters() {
         return searchParamDefs;
     }
-    public Map<String, FHIRR4OperationDef> getOperations(){
+
+    @Override
+    public Map<String, FHIROperationDef> getOperations(){
         return operationDefMap;
     }
-    public void addSearchParam(String url, FHIRR4SearchParamDef searchParam){
-        searchParamDefs.put(url,searchParam);
+
+    @Override
+    public void addSearchParam(String url, FHIRSearchParamDef searchParam) {
+        searchParamDefs.put(url, searchParam);
     }
 
-    public FHIRR4ResourceDef getParentResource() {
+    @Override
+    public FHIRResourceDef<StructureDefinition, Extension, ElementDefinition> getParentResource() {
         return parentResource;
     }
 
-    public void setParentResource(FHIRR4ResourceDef parentResource) {
-        this.parentResource = parentResource;
+    @Override
+    public void setParentResource(FHIRResourceDef<StructureDefinition, Extension, ElementDefinition> parentResource) {
+        this.parentResource = (FHIRR4ResourceDef) parentResource;
     }
 
+    @Override
     public StructureDefinition getDataType(String fhirPath) {
         return null;
     }
@@ -83,16 +95,20 @@ public class FHIRR4ResourceDef extends FHIRResourceDef {
      * @param expansionType values: snapshot|differential
      * @return elements of the FHIR resource
      */
-    public List<ElementDefinition> getElements(R4ElementExpansionType expansionType) {
+    @Override
+    public List<ElementDefinition> getElements(ElementExpansionType expansionType) {
         List<ElementDefinition> elementDefinitions = null;
         List<ElementDefinition> processableElementDefinitions = null;
-        if (expansionType.equals(R4ElementExpansionType.SNAPSHOT)) {
+
+        if (expansionType.equals(ElementExpansionType.SNAPSHOT)) {
             elementDefinitions = definition.getSnapshot().getElement();
             processableElementDefinitions = new ArrayList<>();
-        } else if (expansionType.equals(R4ElementExpansionType.DIFFERENTIAL)) {
+        }
+        else if (expansionType.equals(ElementExpansionType.DIFFERENTIAL)) {
             elementDefinitions = definition.getDifferential().getElement();
             processableElementDefinitions = new ArrayList<>();
         }
+
         if (elementDefinitions != null) {
             for (ElementDefinition elementDefinition : elementDefinitions) {
                 if (FHIRR4SpecUtils.canSkip(definition, elementDefinition)) {
@@ -113,11 +129,11 @@ public class FHIRR4ResourceDef extends FHIRResourceDef {
      *
      * @return Resource type kind
      */
-    public R4DefKind getKind() {
+    public DefKind getKind() {
         return kind;
     }
 
-    public void setKind(R4DefKind kind) {
+    public void setKind(DefKind kind) {
         this.kind = kind;
     }
 }
