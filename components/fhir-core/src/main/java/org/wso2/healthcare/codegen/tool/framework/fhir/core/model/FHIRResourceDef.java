ded7e41372f443246c6bbd351cd1c894773e1408
SA-Tester <siattanayake@gmail.com>
2025-04-28 16:05:26 +0530

    Revert "updated resource definition files to support both r4 and r5 fhir versions"
    This reverts commit 1f46242f4a4498f0e1847029bb7ff9ace7b14fe4.

--- components/fhir-core/src/main/java/org/wso2/healthcare/codegen/tool/framework/fhir/core/model/FHIRResourceDef.java
+++ components/fhir-core/src/main/java/org/wso2/healthcare/codegen/tool/framework/fhir/core/model/FHIRResourceDef.java
@@ -1,46 +1,10 @@
 package org.wso2.healthcare.codegen.tool.framework.fhir.core.model;
 
+import org.hl7.fhir.r4.model.StructureDefinition;
 import org.wso2.healthcare.codegen.tool.framework.commons.model.SpecModel;
-import org.wso2.healthcare.codegen.tool.framework.fhir.core.util.DefKind;
-import org.wso2.healthcare.codegen.tool.framework.fhir.core.util.ElementExpansionType;
 
-import java.util.List;
-import java.util.Map;
-
-// S = StructureDefinition
-// E = Extension
-// L = ElementDefinition
-
-public interface FHIRResourceDef <S, E, L> extends SpecModel {
-
-    S getDefinition();
-
-    void setDefinition(S definition);
-
-    List<E> getExtensions();
-
-    Map<String, FHIRSearchParamDef> getSearchParameters();
-
-    Map<String, FHIROperationDef> getOperations();
-
-    void addSearchParam(String url, FHIRSearchParamDef searchParam);
-
-    FHIRResourceDef <S, E, L> getParentResource();
-
-    void setParentResource(FHIRResourceDef <S, E, L> parentResource);
-
-    S getDataType(String fhirPath);
-
-    List<L> getElements(ElementExpansionType expansionType);
-
-    String getDefinitionType();
-
-    /**
-     * Returns resource type kind. values("resource", "logical", "invalid").
-     *
-     * @return Resource type kind
-     */
-    DefKind getKind();
-
-    void setKind(DefKind kind);
+public class FHIRResourceDef implements SpecModel {
+    public StructureDefinition getDefinition() {
+        return null;
+    }
 }
